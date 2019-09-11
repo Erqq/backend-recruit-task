a0c504044f0ef1efbf8686d051f722427f6b526f
Eerik Timonen <eerik.timonen94@gmail.com>
2019-09-11 23:39:03 +0300

    fix some things

--- src/queries.ts
+++ src/queries.ts
@@ -2,33 +2,38 @@ import User from './models/User';
 import { password , jwt } from './accounts';
 import { BadRequestError } from './Errors';
 
-
-export const userNameExists = async username => {
+/**
+ * Checks if the username na is already in the database
+ * @param username The username to be checked
+ */
+export const userNameExists = async (username) => {
 
   return await User.query().select('username')
     .where('username', username)
-    .then(async uNameList => {
-      if (uNameList.length === 0) {
-        return false;
-      }
-      return true;
-    });
+    .then(async (uNameList) => {
 
+      return uNameList.length === 0 ? false : true;
+    });
 };
 
+/**
+ * Checks if the password is correct
+ * @param username The users username
+ * @param userPassword The password to be checked
+ */
 export const checkPassword = async (username: string, userPassword: string) => {
   return await User.query().select('password')
     .where('username', username).first()
-    .then(async user => {
+    .then(async (user) => {
 
-      if (await password.comparePassword(userPassword, user.password)) {
-        return true;
-      }
-      return false;
-
+      return await password.comparePassword(userPassword, user.password) ? true : false;
     });
 };
 
+/**
+ * Checks if the token is valid and returns user information.
+ * @param token The signed JSON Web token to validate
+ */
 export const checkToken = async (token:string) => {
   try {
     const validToken: any = jwt.validateToken(token);
--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -5,12 +5,21 @@ import { checkToken } from '../queries';
 
 
 export default (router: express.Router) => {
+
+  /**
+   * Gets all the todos of the user if the token is valid.
+   */
   router.get('/todos', async (req, res) => {
     const user = await checkToken(req.body.token);
-    const todos = await Todo.query().where('userid', user.id);
+    const todos: any = await Todo.query().where('userid', user.id);
+
+    if (todos.length === 0) throw new NotFoundError('No Todos!');
     res.send(todos);
   });
 
+  /**
+   * Gets specific todo with the given id.
+   */
   router.get('/todos/:id', async (req, res) => {
     const id = req.params.id;
     if (!id || Number.isInteger(id)) throw new BadRequestError('Invalid TodoID!');
@@ -21,20 +30,19 @@ export default (router: express.Router) => {
     res.send(todo);
   });
 
+/**
+ * Adds a todo.
+ */
   router.post('/todo', async (req, res) => {
     const user: any = await checkToken(req.body.token);
     const todo = req.body.todo;
 
-    try {
-      await Todo.query().insert({
-        title: todo.title,
-        description: `${user.name} remember: ${todo.description}`,
-        userId: user.id,
-      });
-    } catch (error) {
-      console.log(error);
+    await Todo.query().insert({
+      title: todo.title,
+      description: `${user.name} remember: ${todo.description}`,
+      userId: user.id,
+    }).catch(() => { throw new BadRequestError('Could not add new todo'); });
 
-    }
     res.send('Todo added successfully');
   });
 
